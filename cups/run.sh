e17c2b70df2850d4c4fa4d2e97e15e3a9eb3c572
jw59 <joshw59@gmail.com>
2022-04-13 22:49:32 -0400

    Remove firewall rules and HP printer configuration

--- cups/run.sh
+++ cups/run.sh
@@ -3,38 +3,7 @@
 # Tell the container that DBUS should report to Host OS
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
-# Set rules for connection only through the public device URL
-iptables -A INPUT -i resin-vpn -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -i resin-vpn -j ACCEPT
-iptables -A INPUT -p tcp --dport 80 -j REJECT
-
 echo "Starting CUPS"
 cupsd -f &
-
-
-echo "Configuring HP Printers"
-if [[ -n "$HP_IP_PRINTERS" ]]; then
-  printers=$(echo $HP_IP_PRINTERS | tr ";" "\n")
-  for printer in $printers
-  do
-    echo "Make URI for $printer"
-    url=$(hp-makeuri -c $printer)
-    if [ $? -eq 0 ]; then
-      echo "Found URL: $url"
-      echo "Checking to see if printer is in CUPS already"
-      lpstat -v | grep -Fe "$url"
-      if [ $? -ne 0 ]; then
-        echo "Configuring [$printer]"
-        hp-setup -i -a -x $printer
-      else
-        echo "Printer $printer already in CUPS with url: $url"
-      fi
-    else
-      echo "Couldn't find printer on network: $printer"
-    fi 
-  done
-else
-  echo "No HP IP Printers to configure"
-fi
 
 wait
